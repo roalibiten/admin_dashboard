b6004608ec91ab30c5c33390ce0f7b5dd47bd32d
roalibiten <46627141+roalibiten@users.noreply.github.com>
2023-01-22 16:46:23 -0800

    Main Structure

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {Navbar, Footer, Sidebar, ThemeSettings} from './components'
 
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages'
 
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css';
 
 
-function App() {
 
-  const activeMenu=true
+function App() {
+  const {activeMenu} = useStateContext()
 
   return (
     <div className="App">
@@ -31,19 +33,19 @@ function App() {
           </div>
 
           {activeMenu?
-            <div className=' w-72 fixed dark:bg-secondary-dark-bg bg-white'>
+            <div className=' w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
               <Sidebar/>
             </div>
             :
-            <div className=' w-0 dark:bg-secondary-dark-bg'>
+            <div className=' w-0  dark:bg-secondary-dark-bg'>
               <Sidebar/>
             </div>
           }
           <div className={
-            ` dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu? "md:ml-72" : "flex-2"} `
+            ` dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu? "md:ml-72" : "flex-2 "} `
              }
           >
-            <div className=' hidden fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full '>
+            <div className=' fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full '>
               <Navbar/>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { registerLicense } from '@syncfusion/ej2-base';
-
+import { ContextProvider } from './contexts/ContextProvider';
 
 registerLicense('@32302e342e30IAyxM/R5DHv9gLhU7D6KSFh3Y0wd/ xj7xIt/UL5iCQ=;Mgo DSMBaFt/QHRqVVhjVFpFaV1LQmFJfFBmQmlbd1RzdkU3HVdTRHRcQlxhS35UdE1gW3lXcHA=;Mgo DSMBMAY9C3t2VVhkQlFadVdJXHxLd0x0RWFab1l6dl1MY15BNQtUQF1hSn5SdEdjXn1Wc3ZTT2JY;Mgo DSMBPh8sVXJ0S0J XE9HflRDQmFAYVF2R2BJeFRyfF9FYkwgOX1dQl9gSX9RdEVkW3ddcnJcRWQ=;@32302e342e30jFOvISbf3oOYeM2bffr93K3vYvv4Abx41CRRfjp7TGg=;NRAiBiAaIQQuGjN/V0Z WE9EaFxKVmJWfFppR2NbfE53fldOal9TVAciSV9jS31Td0dhWXhceHdXQGlbVQ==;NT8mJyc2IWhhY31nfWN9YGtoYnxhanxhY2Fgc2RpYWtpYGFzEh5oITwyPzonMiE6ODE6JzY9EzQ Mjo/fTA8Pg==;ORg4AjUWIQA/Gnt2VVhkQlFadVdJXHxLd0x0RWFab1l6dl1MY15BNQtUQF1hSn5SdEdjXn1Wc3ZSRmBY;@32302e342e30I3JP H5g8fjPBBist5p1VgU507UDcqnJEL4FEHIFpTg=;@32302e342e30iLDspSTK/oHB6o1N9CWmoBgjoR7ZhD3ydwe PND8XPI=;@32302e342e30IfildLpiQ/58WjiiYg0 GF3BS9h/fFLT5ehB2y9tciI=;@32302e342e30AZWPgvLuGmOEHujMj v Yul470sjPqeYQufczi0LxPE=;@32302e342e30IAyxM/R5DHv9gLhU7D6KSFh3Y0wd/ xj7xIt/UL5iCQ=');
 
@@ -11,5 +11,7 @@ registerLicense('@32302e342e30IAyxM/R5DHv9gLhU7D6KSFh3Y0wd/ xj7xIt/UL5iCQ=;Mgo D
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ContextProvider>
+        <App />
+    </ContextProvider>
 );
